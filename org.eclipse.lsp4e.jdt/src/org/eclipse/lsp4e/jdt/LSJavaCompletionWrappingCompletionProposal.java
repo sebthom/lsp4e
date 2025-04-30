sebthom/lsp4e

commit 1443a66b9c53cb6f91e5fc0f7805ec5e73f070ea

Rework LS completion proposals for JDT

org.eclipse.lsp4e.jdt/src/org/eclipse/lsp4e/jdt/LspJavaQuickAssistProcessor.java
@@ -11,6 +11,8 @@
  *******************************************************************************/
 package org.eclipse.lsp4e.jdt;
 
+import java.util.Arrays;
+
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jdt.annotation.Nullable;
 import org.eclipse.jdt.ui.text.java.IInvocationContext;
@@ -18,12 +20,11 @@ import org.eclipse.jdt.ui.text.java.IJavaCompletionProposal;
 import org.eclipse.jdt.ui.text.java.IProblemLocation;
 import org.eclipse.jdt.ui.text.java.IQuickAssistProcessor;
 import org.eclipse.jface.text.contentassist.ICompletionProposal;
-import org.eclipse.jface.text.contentassist.ICompletionProposalExtension;
-import org.eclipse.jface.text.contentassist.ICompletionProposalExtension2;
 import org.eclipse.jface.text.quickassist.IQuickAssistInvocationContext;
 import org.eclipse.jface.text.source.ISourceViewer;
 import org.eclipse.jface.text.source.TextInvocationContext;
 import org.eclipse.lsp4e.operations.codeactions.LSPCodeActionQuickAssistProcessor;
+import org.eclipse.lsp4e.operations.completion.LSCompletionProposal;
 
 @SuppressWarnings("restriction")
 public class LspJavaQuickAssistProcessor extends LSPCodeActionQuickAssistProcessor implements IQuickAssistProcessor {
@@ -64,21 +65,9 @@ public class LspJavaQuickAssistProcessor extends LSPCodeActionQuickAssistProcess
 		if (proposals == null)
 			return NO_JAVA_COMPLETION_PROPOSALS;
 
-		final var javaProposals = new IJavaCompletionProposal[proposals.length];
-		for (int i = 0; i < proposals.length; i++) {
-			/*
-			 * Different completion extension applied differently, hence each requires a wrapper implementing
-			 * proper completion proposal extension
-			 */
-			if (proposals[i] instanceof ICompletionProposalExtension2) {
-				javaProposals[i] = new LSJavaProposalExtension2(proposals[i]);
-			} else if (proposals[i] instanceof ICompletionProposalExtension) {
-				javaProposals[i] = new LSJavaProposalExtension(proposals[i]);
-			} else {
-				javaProposals[i] = new LSJavaProposal(proposals[i]);
-			}
-		}
-		return javaProposals;
+		return Arrays.stream(proposals).filter(LSCompletionProposal.class::isInstance)
+				.map(LSCompletionProposal.class::cast).map(LSJavaProposal::new).toArray(LSJavaProposal[]::new);
+
 	}
 
 }

org.eclipse.lsp4e.jdt/META-INF/MANIFEST.MF
@@ -2,7 +2,7 @@ Manifest-Version: 1.0
 Bundle-ManifestVersion: 2
 Bundle-Name: JDT Integration for LSP4E
 Bundle-SymbolicName: org.eclipse.lsp4e.jdt;singleton:=true
-Bundle-Version: 0.13.5.qualifier
+Bundle-Version: 0.13.6.qualifier
 Automatic-Module-Name: org.eclipse.lsp4e.jdt
 Bundle-RequiredExecutionEnvironment: JavaSE-17
 Bundle-ClassPath: .

org.eclipse.lsp4e.jdt/src/org/eclipse/lsp4e/jdt/LSJavaCompletionProposalComputer.java
@@ -11,6 +11,7 @@
  *******************************************************************************/
 package org.eclipse.lsp4e.jdt;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
@@ -22,10 +23,9 @@ import org.eclipse.jdt.annotation.Nullable;
 import org.eclipse.jdt.ui.text.java.ContentAssistInvocationContext;
 import org.eclipse.jdt.ui.text.java.IJavaCompletionProposalComputer;
 import org.eclipse.jface.text.contentassist.ICompletionProposal;
-import org.eclipse.jface.text.contentassist.ICompletionProposalExtension;
-import org.eclipse.jface.text.contentassist.ICompletionProposalExtension2;
 import org.eclipse.jface.text.contentassist.IContextInformation;
 import org.eclipse.lsp4e.LanguageServerPlugin;
+import org.eclipse.lsp4e.operations.completion.LSCompletionProposal;
 import org.eclipse.lsp4e.operations.completion.LSContentAssistProcessor;
 
 @SuppressWarnings({ "restriction" })
@@ -51,7 +51,7 @@ public class LSJavaCompletionProposalComputer implements IJavaCompletionProposal
 			lsContentAssistProcessor.computeCompletionProposals(viewer, context.getInvocationOffset()));
 
 		try {
-			return List.of(asJavaProposals(future));
+			return List.of(asJavaProposals(future, context));
 		} catch (ExecutionException | TimeoutException e) {
 			LanguageServerPlugin.logError(e);
 			javaCompletionSpecificErrorMessage = createErrorMessage(e);
@@ -77,21 +77,12 @@ public class LSJavaCompletionProposalComputer implements IJavaCompletionProposal
 	 * This method wraps around the LSCompletionProposal with a IJavaCompletionProposal, and it sets the relevance
 	 * number that JDT uses to sort proposals in a desired order.
 	 */
-	private ICompletionProposal[] asJavaProposals(CompletableFuture<ICompletionProposal[]> future)
+	private ICompletionProposal[] asJavaProposals(CompletableFuture<ICompletionProposal[]> future, ContentAssistInvocationContext context)
 			throws InterruptedException, ExecutionException, TimeoutException {
 		ICompletionProposal[] originalProposals = future.get(TIMEOUT_LENGTH, TIMEOUT_UNIT);
 
-		final var javaProposals = new ICompletionProposal[originalProposals.length];
-		for (int i = 0; i < originalProposals.length; i++) {
-			if (originalProposals[i] instanceof ICompletionProposalExtension2) {
-				javaProposals[i] = new LSJavaProposalExtension2(originalProposals[i]);
-			} else if (originalProposals[i] instanceof ICompletionProposalExtension) {
-				javaProposals[i] = new LSJavaProposalExtension(originalProposals[i]);
-			} else {
-				javaProposals[i] = new LSJavaProposal(originalProposals[i]);
-			}
-		}
-		return javaProposals;
+		return Arrays.stream(originalProposals).filter(LSCompletionProposal.class::isInstance).map(LSCompletionProposal.class::cast).map(LSJavaProposal::new).toArray(LSJavaProposal[]::new);
+		
 	}
 
 	@Override

org.eclipse.lsp4e.jdt/src/org/eclipse/lsp4e/jdt/LSJavaProposal.java
@@ -11,18 +11,37 @@
  *******************************************************************************/
 package org.eclipse.lsp4e.jdt;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jdt.annotation.Nullable;
 import org.eclipse.jdt.internal.codeassist.RelevanceConstants;
+import org.eclipse.jdt.internal.ui.text.java.AbstractJavaCompletionProposal;
+import org.eclipse.jdt.internal.ui.text.java.hover.JavadocBrowserInformationControlInput;
+import org.eclipse.jdt.internal.ui.text.java.hover.JavadocHover;
+import org.eclipse.jdt.ui.PreferenceConstants;
 import org.eclipse.jdt.ui.text.java.IJavaCompletionProposal;
-import org.eclipse.jface.text.IDocument;
-import org.eclipse.jface.text.contentassist.ICompletionProposal;
-import org.eclipse.jface.text.contentassist.IContextInformation;
+import org.eclipse.jface.internal.text.html.BrowserInformationControl;
+import org.eclipse.jface.internal.text.html.HTMLPrinter;
+import org.eclipse.jface.resource.ColorRegistry;
+import org.eclipse.jface.resource.JFaceResources;
+import org.eclipse.jface.text.IInformationControlCreator;
+import org.eclipse.lsp4e.LanguageServerPlugin;
 import org.eclipse.lsp4e.operations.completion.LSCompletionProposal;
-import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.graphics.Point;
+import org.eclipse.lsp4e.ui.UI;
+import org.eclipse.swt.graphics.FontData;
+import org.eclipse.swt.graphics.RGB;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IWorkbenchPart;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
 
 @SuppressWarnings("restriction")
-class LSJavaProposal implements IJavaCompletionProposal {
+class LSJavaProposal extends LSCompletionProposal implements IJavaCompletionProposal {
 	
 	private static final int LS_DEFAULT_RELEVANCE = 18;
 	
@@ -33,51 +52,23 @@ class LSJavaProposal implements IJavaCompletionProposal {
 
 	private static final int RANGE_WITHIN_CATEGORY = Math.round((MAX_BASE_RELEVANCE - DEFAULT_RELEVANCE) / 4f);
 
-	protected ICompletionProposal delegate;
+	private static @Nullable String fgCSSStyles = null;
+	
 	private boolean relevanceComputed = false;
 	private int relevance = -1;
-	
-	public LSJavaProposal(ICompletionProposal delegate) {
-		this.delegate = delegate;
-	}
 
-	@Override
-	public void apply(IDocument document) {
-		delegate.apply(document);
-	}
+	private @Nullable IInformationControlCreator infoControlCreator = null;
 
-	@Override
-	public @Nullable String getAdditionalProposalInfo() {
-		return delegate.getAdditionalProposalInfo();
-	}
-
-	@Override
-	public @Nullable IContextInformation getContextInformation() {
-		return delegate.getContextInformation();
-	}
-
-	@Override
-	public String getDisplayString() {
-		return delegate.getDisplayString();
-	}
-
-	@Override
-	public @Nullable Image getImage() {
-		return delegate.getImage();
-	}
-
-	@Override
-	public @Nullable Point getSelection(IDocument document) {
-		return delegate.getSelection(document);
+	public LSJavaProposal(LSCompletionProposal lsProposal) {
+		super(lsProposal);
 	}
 
 	@Override
 	public int getRelevance() {
 		if (!relevanceComputed) {
-			if (delegate instanceof LSCompletionProposal c) {
 				// Based on org.eclipse.jdt.internal.ui.text.java.RelevanceComputer
-				relevance = computeBaseRelevance(c);
-				switch (c.getItem().getKind()) {
+				relevance = computeBaseRelevance();
+				switch (getItem().getKind()) {
 				case Class:
 					relevance += 3;
 					break;
@@ -94,18 +85,96 @@ class LSJavaProposal implements IJavaCompletionProposal {
 					break;
 				default:
 				}
-			}
 			relevanceComputed = true;
 		}
 		return relevance;
 	}
 	
-	private int computeBaseRelevance(LSCompletionProposal c) {
+	private int computeBaseRelevance() {
 		// Incorporate LSP4E category and rank into base relevance.
-		int base = MAX_BASE_RELEVANCE - (c.getRankCategory() - 1) * RANGE_WITHIN_CATEGORY;
-		int rank = c.getRankScore();
+		int base = MAX_BASE_RELEVANCE - (getRankCategory() - 1) * RANGE_WITHIN_CATEGORY;
+		int rank = getRankScore();
 		base -= (rank >= 0 && rank < RANGE_WITHIN_CATEGORY ? rank : RANGE_WITHIN_CATEGORY);
 		return base;
 	}
+	
+	/*
+	 * Copied from org.eclipse.jdt.internal.ui.text.java.AbstractJavaCompletionProposal.getInformationControlCreator()
+	 */
+	@Override
+	public @Nullable IInformationControlCreator getInformationControlCreator() {
+		Shell shell= UI.getActiveShell();
+		if (shell == null || !BrowserInformationControl.isAvailable(shell))
+			return null;
+
+		if (infoControlCreator == null) {
+			/*
+			 * see https://bugs.eclipse.org/bugs/show_bug.cgi?id=232024
+			 */
+			IWorkbenchPart part = UI.getActivePart();
+			JavadocHover.PresenterControlCreator presenterControlCreator= new JavadocHover.PresenterControlCreator(part == null ? null : part.getSite());
+			infoControlCreator= new JavadocHover.HoverControlCreator(presenterControlCreator, true);
+		}
+		return infoControlCreator;
+	}
+
+	/*
+	 * Copied from org.eclipse.jdt.internal.ui.text.java.AbstractJavaCompletionProposal.getAdditionalProposalInfo(IProgressMonitor monitor)
+	 */
+	@Override
+	public Object getAdditionalProposalInfo(IProgressMonitor monitor) {
+		StringBuilder buffer = new StringBuilder((String) super.getAdditionalProposalInfo(monitor));
+		ColorRegistry registry= JFaceResources.getColorRegistry();
+		RGB fgRGB= registry.getRGB("org.eclipse.jdt.ui.Javadoc.foregroundColor"); //$NON-NLS-1$
+		RGB bgRGB= registry.getRGB("org.eclipse.jdt.ui.Javadoc.backgroundColor"); //$NON-NLS-1$
+		HTMLPrinter.insertPageProlog(buffer, 0, fgRGB, bgRGB, getCSSStyles());
+		HTMLPrinter.addPageEpilog(buffer);
+		return new JavadocBrowserInformationControlInput(null, null, buffer.toString(), 0);
+
+	}
+	
+	/**
+	 * Returns the style information for displaying HTML (Javadoc) content.
+	 * Copied from org.eclipse.jdt.internal.ui.text.java.AbstractJavaCompletionProposal.getCSSStyles()
+	 *
+	 * @return the CSS styles
+	 */
+	private @Nullable String getCSSStyles() {
+		if (fgCSSStyles == null) {
+			Bundle bundle= FrameworkUtil.getBundle(AbstractJavaCompletionProposal.class);
+			URL url= bundle == null ? null : bundle.getEntry("/JavadocHoverStyleSheet.css"); //$NON-NLS-1$
+			if (url != null) {
+				BufferedReader reader= null;
+				try {
+					url= FileLocator.toFileURL(url);
+					reader= new BufferedReader(new InputStreamReader(url.openStream()));
+					StringBuilder buffer= new StringBuilder(200);
+					String line= reader.readLine();
+					while (line != null) {
+						buffer.append(line);
+						buffer.append('\n');
+						line= reader.readLine();
+					}
+					fgCSSStyles= buffer.toString();
+				} catch (IOException ex) {
+					LanguageServerPlugin.logError(ex);
+				} finally {
+					try {
+						if (reader != null)
+							reader.close();
+					} catch (IOException e) {
+					}
+				}
+
+			}
+		}
+		String css= fgCSSStyles;
+		if (css != null) {
+			FontData fontData= JFaceResources.getFontRegistry().getFontData(PreferenceConstants.APPEARANCE_JAVADOC_FONT)[0];
+			css= HTMLPrinter.convertTopLevelFont(css, fontData);
+		}
+		return css;
+	}
+
 
 }

org.eclipse.lsp4e.jdt/src/org/eclipse/lsp4e/jdt/LSJavaProposalExtension.java
@@ -1,56 +1,0 @@
-/*******************************************************************************
- * Copyright (c) 2024 Broadcom Inc. and others.
- * This program and the accompanying materials are made
- * available under the terms of the Eclipse Public License 2.0
- * which is available at https://www.eclipse.org/legal/epl-2.0/
- *
- * SPDX-License-Identifier: EPL-2.0
- *
- * Contributors:
- *  - Alex Boyko (VMware Inc.) - Initial implementation
- *******************************************************************************/
-package org.eclipse.lsp4e.jdt;
-
-import org.eclipse.jdt.annotation.Nullable;
-import org.eclipse.jface.text.IDocument;
-import org.eclipse.jface.text.contentassist.ICompletionProposal;
-import org.eclipse.jface.text.contentassist.ICompletionProposalExtension;
-
-class LSJavaProposalExtension extends LSJavaProposal implements ICompletionProposalExtension {
-
-	public LSJavaProposalExtension(ICompletionProposal delegate) {
-		super(delegate);
-	}
-
-	@Override
-	public void apply(IDocument doc, char trigger, int offset) {
-		if (delegate instanceof ICompletionProposalExtension proposalExt) {
-			proposalExt.apply(doc, trigger, offset);
-		}
-	}
-
-	@Override
-	public int getContextInformationPosition() {
-		if (delegate instanceof ICompletionProposalExtension proposalExt) {
-			return proposalExt.getContextInformationPosition();
-		}
-		return -1;
-	}
-
-	@Override
-	public char @Nullable [] getTriggerCharacters() {
-		if (delegate instanceof ICompletionProposalExtension proposalExt) {
-			return proposalExt.getTriggerCharacters();
-		}
-		return null;
-	}
-
-	@Override
-	public boolean isValidFor(IDocument doc, int offset) {
-		if (delegate instanceof ICompletionProposalExtension proposalExt) {
-			return proposalExt.isValidFor(doc, offset);
-		}
-		return false;
-	}
-
-}

org.eclipse.lsp4e.jdt/src/org/eclipse/lsp4e/jdt/LSJavaProposalExtension2.java
@@ -1,56 +1,0 @@
-/*******************************************************************************
- * Copyright (c) 2024 Broadcom Inc. and others.
- * This program and the accompanying materials are made
- * available under the terms of the Eclipse Public License 2.0
- * which is available at https://www.eclipse.org/legal/epl-2.0/
- *
- * SPDX-License-Identifier: EPL-2.0
- *
- * Contributors:
- *  - Alex Boyko (VMware Inc.) - Initial implementation
- *******************************************************************************/
-package org.eclipse.lsp4e.jdt;
-
-import org.eclipse.jdt.annotation.Nullable;
-import org.eclipse.jface.text.DocumentEvent;
-import org.eclipse.jface.text.IDocument;
-import org.eclipse.jface.text.ITextViewer;
-import org.eclipse.jface.text.contentassist.ICompletionProposal;
-import org.eclipse.jface.text.contentassist.ICompletionProposalExtension2;
-
-class LSJavaProposalExtension2 extends LSJavaProposal implements ICompletionProposalExtension2 {
-
-	public LSJavaProposalExtension2(ICompletionProposal delegate) {
-		super(delegate);
-	}
-
-	@Override
-	public void apply(ITextViewer viewer, char trigger, int stateMask, int offset) {
-		if (delegate instanceof ICompletionProposalExtension2 proposalExt2) {
-			proposalExt2.apply(viewer, trigger, stateMask, offset);
-		}
-	}
-
-	@Override
-	public void selected(ITextViewer viewer, boolean smartToggle) {
-		if (delegate instanceof ICompletionProposalExtension2 proposalExt2) {
-			proposalExt2.selected(viewer, smartToggle);
-		}
-	}
-
-	@Override
-	public void unselected(ITextViewer viewer) {
-		if (delegate instanceof ICompletionProposalExtension2 proposalExt2) {
-			proposalExt2.unselected(viewer);
-		}
-	}
-
-	@Override
-	public boolean validate(IDocument document, int offset, @Nullable DocumentEvent event) {
-		if (delegate instanceof ICompletionProposalExtension2 proposalExt2) {
-			return proposalExt2.validate(document, offset, event);
-		}
-		return false;
-	}
-
-}

org.eclipse.lsp4e.test/META-INF/MANIFEST.MF
@@ -2,7 +2,7 @@ Manifest-Version: 1.0
 Bundle-ManifestVersion: 2
 Bundle-Name: Tests for language server bundle (Incubation)
 Bundle-SymbolicName: org.eclipse.lsp4e.test;singleton:=true
-Bundle-Version: 0.15.24.qualifier
+Bundle-Version: 0.15.25.qualifier
 Fragment-Host: org.eclipse.lsp4e
 Bundle-Vendor: Eclipse LSP4E
 Bundle-RequiredExecutionEnvironment: JavaSE-17

org.eclipse.lsp4e.test/pom.xml
@@ -8,7 +8,7 @@
 	</parent>
 	<artifactId>org.eclipse.lsp4e.test</artifactId>
 	<packaging>eclipse-test-plugin</packaging>
-	<version>0.15.24-SNAPSHOT</version>
+	<version>0.15.25-SNAPSHOT</version>
 
 	<properties>
 		<os-jvm-flags /> <!-- for the default case -->

commit 9c26ffa1ba6adab114ff2ee0204b68f104dfd65c

JDT completions compatible with any `ICompletionProposal` (#1256)
JDT completions changes 1443a66b9c53cb6f91e5fc0f7805ec5e73f070ea broke
CodeAction for JDT coming from LS. This change should restore the
behaviour for CodeActions.

org.eclipse.lsp4e.jdt/src/org/eclipse/lsp4e/jdt/LSJavaCompletionWrappingCompletionProposal.java
@@ -1,0 +1,65 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Broadcom Inc. and others.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *  - Alex Boyko (Broadcom Inc.) - Initial implementation
+ *******************************************************************************/
+package org.eclipse.lsp4e.jdt;
+
+import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.jdt.ui.text.java.IJavaCompletionProposal;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.contentassist.ICompletionProposal;
+import org.eclipse.jface.text.contentassist.IContextInformation;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.Point;
+
+class LSJavaCompletionWrappingCompletionProposal implements IJavaCompletionProposal {
+	
+	private final ICompletionProposal delegate;
+	
+	public LSJavaCompletionWrappingCompletionProposal(ICompletionProposal delegate) {
+		this.delegate = delegate;
+	}
+
+	@Override
+	public void apply(IDocument document) {
+		delegate.apply(document);
+	}
+
+	@Override
+	public @Nullable Point getSelection(IDocument document) {
+		return delegate.getSelection(document);
+	}
+
+	@Override
+	public @Nullable String getAdditionalProposalInfo() {
+		return delegate.getAdditionalProposalInfo();
+	}
+
+	@Override
+	public String getDisplayString() {
+		return delegate.getDisplayString();
+	}
+
+	@Override
+	public @Nullable Image getImage() {
+		return delegate.getImage();
+	}
+
+	@Override
+	public @Nullable IContextInformation getContextInformation() {
+		return delegate.getContextInformation();
+	}
+
+	@Override
+	public int getRelevance() {
+		return -1;
+	}
+
+}

org.eclipse.lsp4e.jdt/src/org/eclipse/lsp4e/jdt/LspJavaQuickAssistProcessor.java
@@ -64,10 +64,13 @@ public class LspJavaQuickAssistProcessor extends LSPCodeActionQuickAssistProcess
 		ICompletionProposal[] proposals = computeQuickAssistProposals(getContext(context));
 		if (proposals == null)
 			return NO_JAVA_COMPLETION_PROPOSALS;
-
-		return Arrays.stream(proposals).filter(LSCompletionProposal.class::isInstance)
-				.map(LSCompletionProposal.class::cast).map(LSJavaProposal::new).toArray(LSJavaProposal[]::new);
-
+		
+		return Arrays.stream(proposals).map(p -> {
+			if (p instanceof LSCompletionProposal lscp) {
+				return new LSJavaProposal(lscp);
+			}
+			return new LSJavaCompletionWrappingCompletionProposal(p);
+		}).toArray(IJavaCompletionProposal[]::new);
 	}
 
 }
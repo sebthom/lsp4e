ed036b0a494ea8537cd30fb9c0e3bce49273c313
Sebastian Thomschke <sebthom@users.noreply.github.com>
2025-11-18 21:13:07 +0100

    fix: mitigate random test failures on Windows because of locked resource

--- org.eclipse.lsp4e.test/src/org/eclipse/lsp4e/test/utils/AbstractTestWithProject.java
+++ org.eclipse.lsp4e.test/src/org/eclipse/lsp4e/test/utils/AbstractTestWithProject.java
@@ -15,6 +15,7 @@ import java.lang.reflect.Method;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestInfo;
@@ -35,8 +36,38 @@ public abstract class AbstractTestWithProject extends AbstractTest {
 
 	@AfterEach
 	public void tearDownProject() throws Exception {
-		if (project != null) {
-			project.delete(IResource.FORCE, null);
+		if (project != null && project.exists()) {
+			deleteProjectWithRetries(project, 10, 500);
+		}
+	}
+
+	/**
+	 * Mitigation for potential
+	 * <code>java.nio.file.FileSystemException: The process cannot access the file because it is being used by another process</code>
+	 * when deleting a project.
+	 */
+	private static void deleteProjectWithRetries(IProject project, int maxAttempts, long delayMillis)
+			throws CoreException {
+		for (int attempt = 1; attempt <= maxAttempts; attempt++) {
+			try {
+				if (!project.exists()) {
+					break;
+				}
+				project.close(null);
+				project.delete(IResource.FORCE | IResource.ALWAYS_DELETE_PROJECT_CONTENT, null);
+				break;
+			} catch (CoreException ex) {
+				if (attempt == maxAttempts) {
+					throw ex;
+				}
+				try {
+					Thread.sleep(delayMillis);
+				} catch (InterruptedException ie) {
+					Thread.currentThread().interrupt();
+					ex.printStackTrace();
+					break;
+				}
+			}
 		}
 	}
 }
